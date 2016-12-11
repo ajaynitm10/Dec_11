8e32af2e1f737ea8244c2b49219a7efa2ffdac76
ajaynitm10 <Ajaykumar.Mittakola@mindtree.com>
2016-12-12 00:13:59 +0530

    app.js updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //express js 
 
 var exp = require('express');
-var chalk = reuire('chalk');
+var chalk = require('chalk');
 var app = exp();// initiating the web server
 
 app.get('/',function(req,res){
@@ -12,6 +12,7 @@ app.get('/home',function(req,res){
     res.send("this is home path");//send means write() and end()
 });
 
+var port = process.env.PORT || 3000;
 app.listen(3000,function(){
     console.log('server is running on port 3000');
 })
